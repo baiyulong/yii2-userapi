cc6b5086ccdd48a67c56a44005fbf0c4830a78b6
baiyulong <baiyulong@dratio.com>
2015-04-02 20:04:38 +0800

    添加支持配置文件

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace uapi\userapi\controller;
+namespace uapi\userapi\controllers;
 
 use Yii;
 use yii\web\Controller;
@@ -8,11 +8,18 @@ use yii\web\Controller;
 class AuthController extends Controller
 {
 	public function actionSignup()
-	{}
+	{
+		print_r(Yii::$app->getParams());exit;
+		echo 'This is signup action';
+	}
 
 	public function actionLogin()
-	{}
+	{
+		echo 'This is login action';
+	}
 
 	public function actionLogout()
-	{}
+	{
+		echo 'This is logout action';
+	}
 }
